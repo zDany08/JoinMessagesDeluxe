cd0718a9c05c24b5369500ec52fdbec997ea9a55
zDany08 <99984957+zDany08@users.noreply.github.com>
2023-03-02 16:49:55 +0100

    v1.1.1

--- src/main/java/net/zdany/joinmessagesdeluxe/listeners/Quit.java
+++ src/main/java/net/zdany/joinmessagesdeluxe/listeners/Quit.java
@@ -14,6 +14,7 @@ public class Quit implements Listener {
     @EventHandler(priority = EventPriority.LOWEST)
     public void onQuit(PlayerQuitEvent e) {
         Player p = e.getPlayer();
+        if(!p.hasPermission("joinmessagesdeluxe.public.join-quit")) return;
         if(!JoinMessagesDeluxe.getInstance().getConfig().getBoolean("join-messages.public.join-quit.enabled")) return;
         e.setQuitMessage(Format.getColor(PlaceholderAPI.setPlaceholders(p, JoinMessagesDeluxe.getInstance().getConfig().getString("join-messages.public.join-quit.quit"))));
     }
